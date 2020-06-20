4adb1c4cbac8860caa81cbf4b8aa7dea880a27ac
Paweł Czaplewski <pwlczpl@gmail.com>
2020-06-20 13:36:36 +0200

    open a file provided in the command line argument
    Signed-off-by: Paweł Czaplewski <pwlczpl@gmail.com>

--- main.rb
+++ main.rb
@@ -1,3 +1,14 @@
+#!/usr/bin/env ruby
 
-FILE = File.read('brainfuck-program.bf')
+if ARGV.length != 1
+    puts "Please provide the path to brainfuck program"
+    exit
+end
+bf_filename = ARGV[0]
+unless File.exist?(bf_filename)
+    puts "File %s doesnt exist" % [bf_filename]
+    exit
+end
+
+FILE = File.read(bf_filename)
 puts FILE
